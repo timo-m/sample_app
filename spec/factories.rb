f0063f49e008e7423ce636f06749899633e7f9ee
timo-m <timo.martens@yahoo.de>
2011-06-16 08:50:31 +0200

    Done with sign in

--- spec/factories.rb
+++ spec/factories.rb
@@ -1,7 +1,7 @@
 # By using the symbol ':user', we get Factory Girl to simulate the User model.
 Factory.define :user do |user|
-  user.name                   "Timo Martens"
-  user.email                  "timo.martens@yahoo.de"
+  user.name                   "Rails Tutorial"
+  user.email                  "example@railstutorial.org"
   user.password               "foobar"
   user.password_confirmation  "foobar"
 end
